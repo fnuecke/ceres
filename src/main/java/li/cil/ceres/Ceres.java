24f4dd138d5c304289eb9cf14fdfe005aee322d9
Florian Nücke <florian@nuecke.de>
2020-10-02 17:13:13 +0200

    Use Reflections lib to automatically find serializer implementations.

--- src/main/java/li/cil/ceres/Ceres.java
+++ src/main/java/li/cil/ceres/Ceres.java
@@ -1,20 +1,52 @@
 package li.cil.ceres;
 
+import li.cil.ceres.api.RegisterSerializer;
 import li.cil.ceres.api.SerializationException;
 import li.cil.ceres.api.Serializer;
 import li.cil.ceres.internal.SerializerFactory;
 import li.cil.ceres.serializers.ArraySerializer;
-import li.cil.ceres.serializers.UUIDSerializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.reflections.Reflections;
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 public final class Ceres {
+    private static final Logger LOGGER = LogManager.getLogger();
+
     private static final Map<Class<?>, Serializer<?>> SERIALIZERS = new HashMap<>();
 
     static {
-        putSerializer(UUID.class, UUIDSerializer.INSTANCE);
+        for (final Class<?> type : new Reflections().getSubTypesOf(Serializer.class)) {
+            if (!type.isAnnotationPresent(RegisterSerializer.class)) {
+                continue;
+            }
+
+            try {
+                final ArrayList<Class<?>> supportedTypes = new ArrayList<>();
+                Arrays.stream(type.getGenericInterfaces()).forEach(interfaceType -> {
+                    final ParameterizedType parameterizedType = (ParameterizedType) interfaceType;
+                    if (parameterizedType.getRawType() == Serializer.class) {
+                        final Type[] typeArguments = parameterizedType.getActualTypeArguments();
+                        assert typeArguments.length == 1;
+                        supportedTypes.add((Class<?>) typeArguments[0]);
+                    }
+                });
+                if (!supportedTypes.isEmpty()) {
+                    final Serializer<?> serializer = (Serializer<?>) type.newInstance();
+                    for (final Class<?> supportedType : supportedTypes) {
+                        SERIALIZERS.put(supportedType, serializer);
+                    }
+                }
+            } catch (final InstantiationException | IllegalAccessException e) {
+                LOGGER.error("Failed instantiating serializer [{}]: {}", type, e);
+            }
+        }
     }
 
     @SuppressWarnings("unchecked")
